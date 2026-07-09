912435ed6a1b09b0fac4ed1041c1251bc2ec44d8
Pradeep Yadavalli(UST <216634@ust.com>
2026-07-09 15:17:47 +0530

    adding flyway and DB config

---
+++ order-service/src/main/java/com/pradeep/orderservice/entity/Role.java
@@ -0,0 +1,25 @@
+package com.pradeep.orderservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private boolean enabled = true;
+}
---
+++ order-service/src/main/java/com/pradeep/orderservice/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.pradeep.orderservice.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository {
+
+}
